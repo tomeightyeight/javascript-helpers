e8e360962ba385cb6fd815f009990be551bb9f89
Houston VPS <tom@muska.co.uk>
2015-06-09 13:49:31 +0400

    Added default Grunt template (SASS, concat, uglify, watch)

---
+++ basic-grunt-template.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				
+				files: {
+					'./templates/global/css/styles.css': './templates/global/css/styles.scss'
+				}	
+			},
+		},
+		
+		concat: {
+			options: {
+				sourceMap: true,
+				separator: ';'
+			},
+
+			dist: {
+				src: ['./templates/global/js/**/*.js'],
+				
+				dest: './templates/global/js/built/main.js'
+			}
+		},
+		
+        uglify: {
+			options: {
+				banner: '/* <%= pkg.name %>, built <%=  grunt.template.today() %> */\n',
+				compress: true,
+				mangle: true,
+				sourceMap: true
+			},
+			
+			dist: {
+				files: {
+					'./templates/global/js/built/main.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		},
+		
+		watch: {
+			sass: {
+				files: ['./templates/global/css/styles.scss'],
+				tasks: ['sass']				
+			},
+			
+			js: {
+				files: ['./templates/global/js/**/*.js'],
+				tasks: ['concat', 'uglify']
+			}
+		}
+	});
+
+	// Load the plugins that prvide the various Grunt tasks
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task(s).
+	grunt.registerTask('default', ['sass', 'concat', 'uglify']);
+}
